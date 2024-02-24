5180617d30558d0456d4aed4eac50d9685f93db0
Jhostyn <Jhostynchacon2@gmail.com>
2024-02-23 22:56:17 -0600

    se agregó el programa de par o impar haciendo uso de mod para poder verficar el residuo

---
+++ Par o impar/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Ingrese un número: ");
+
+if (int.TryParse(Console.ReadLine(), out int numero))//siempre para evitar que el usuario ingrese un valor no valido
+{
+    if (numero % 2 == 0)//mod para poder extraer el residuo de la division y ver si es igual a 0
+    {
+        Console.WriteLine($"El número {numero} es par.");
+    }
+    else
+    {
+        Console.WriteLine($"El número {numero} es impar.");
+    }
+}
+else
+{
+    Console.WriteLine("Por favor, ingrese un número válido (número entero).");
+}
