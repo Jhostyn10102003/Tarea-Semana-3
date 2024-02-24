e1c53ca4bae2a11e92d058efe1837c81638e08b0
Jhostyn <Jhostynchacon2@gmail.com>
2024-02-23 23:02:31 -0600

    Programa de aprobación de prestamo, con dos condicionales y el uso de ||

---
+++ aprobación de prestamo/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Ingrese la cantidad a prestar: ");
+if (decimal.TryParse(Console.ReadLine(), out decimal Mprestamo))//1er if
+{
+    Console.Write("Ingrese su edad: ");
+    if (int.TryParse(Console.ReadLine(), out int edad)) //usamos || para combinar las condiciones de ambos iff
+    {
+        if (Mprestamo < 5000 || edad > 60)//2do if
+        {
+            Console.WriteLine("¡Préstamo aprobado!");
+        }
+        else
+        {
+            Console.WriteLine("Préstamo rechazado.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Por favor, ingrese una edad válida (número entero).");
+    }
+}
+else
+{
+    Console.WriteLine("Por favor, ingrese un monto válido (número decimal).");
+}
