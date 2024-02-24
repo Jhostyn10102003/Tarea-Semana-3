17e5b6a7f7b702b42b856d176619a3f858c896fe
Jhostyn <Jhostynchacon2@gmail.com>
2024-02-23 19:43:48 -0600

    Agregar archivos de proyecto.

---
+++ Tarea Semana 3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Ingrese el primer número:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el tercer número:");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+double mayor;
+
+if (num1 >= num2 && num1 >= num3)
+{
+    mayor = num1;
+}
+else if (num2 >= num1 && num2 >= num3)
+{
+    mayor = num2;
+}
+else
+{
+    mayor = num3;
+}
+
+Console.WriteLine($"El mayor de los tres números es: {mayor}");
