77734365bdc6d84946e7366205d35ddce9e9c3d4
Jhostyn <Jhostynchacon2@gmail.com>
2024-02-23 22:49:50 -0600

    Se añiadió el 3er programa de validación de usuario

---
+++ Usuariovalidación/Program.cs
@@ -0,0 +1,21 @@
+﻿string Usuario = "Jhostyn";
+string contraseña = "contraseña1";
+
+
+Console.Write("Ingrese el nombre de usuario: ");
+string Usuarioing = Console.ReadLine();   
+
+
+Console.Write("Ingrese la contraseña: ");
+string Contraseñaing = Console.ReadLine();
+
+// Validar las credenciales
+if (Usuarioing == Usuario && Contraseñaing == contraseña)
+{
+    Console.WriteLine("Bienvenido");
+}
+else
+{
+    Console.WriteLine("Los datos proporcionados no son correctos... ");//se podría mejorar añiadiendo un ciclo para que se repita la acción hasta que se escriban los datos validos
+
+}
